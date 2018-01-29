cde8cb2ca9299a1338e76e4a274783717cc91e27
Djamilya007 <magomedova_dgamka@mail.ru>
2018-01-29 16:48:52 +0300

    Add files via upload

---
+++ kortedz.py
@@ -0,0 +1,13 @@
+user_pas = () # пустой кортеж
+user_pas = ('12345','2302435', '43344567')
+print(user_pas)
+
+my_tuple = 1, 2, 3
+print(my_tuple)
+little_tuple = 1,
+print(little_tuple)
+first, second, third = user_pas
+print(first, second, third)
+v_1, v_2, v_3, v_4 = '!', '?', '!?', '??!!'
+print(v_1, v_2, v_3, v_4)
+print(list(user_pas))
