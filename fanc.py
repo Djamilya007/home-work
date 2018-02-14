cdd68386a3ebd2abd77f8ec395e998f2962c150d
Djamilya007 <magomedova_dgamka@mail.ru>
2018-02-14 16:48:24 +0300

    Add files via upload

---
+++ fanc.py
@@ -0,0 +1,18 @@
+def summ():
+	a = int(input('1-е число:'))
+	b = int(input('2-e число:'))
+	print(a + b)
+
+# for i in range(100):
+#     summ()
+def mult(a, b):
+	return(a + b)/2
+# mult(3, 4, 5)
+# mult(10, 20, 30)
+# mult(22, 33, 44)
+# mult(55, 66, 77)
+n = 5
+n2 = 4
+n3 = 3
+n4 = median(3,5)
+print(n4)
