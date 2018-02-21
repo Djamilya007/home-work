af289834a199b936b8c758e7a874186d73d1f602
Djamilya007 <magomedova_dgamka@mail.ru>
2018-02-21 16:32:18 +0300

    Add files via upload

---
+++ modules.py
@@ -0,0 +1,9 @@
+# import random 
+from time import sleep
+from math import sqrt as r_sqrt
+from cmath import sqrt
+from random import *
+print(randint(1,10))
+sleep(3)
+print(randint(1,10))
+print(sqrt(16))
