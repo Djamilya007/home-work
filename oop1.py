ff558d7fc0cc0f638ad45eff3e23595df2d798b4
Djamilya007 <magomedova_dgamka@mail.ru>
2018-03-05 10:50:09 +0000

    Update oop1.py

--- oop1.py
+++ oop1.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 class Person:
-	def __init__(self, name, job=None, pay=0, age = 0):
+	def __init__(self, name, age, job, pay):
 		self.name = name
+		self.age = age
 		self.job = job
 		self.pay = pay
-		self.age = age
 	def lastName(self):
-		return self.name.split()[-1]
+	 	return self.name.split()[-1]
 	def __str__(self):
 	 	return '[Person: %s, %s]' % (self.name, self.pay)
  
-ivan = Person('Иван Petrov')
-john = Person('John Sidorov', job='ФСБ', pay=1000000)
-print(ivan)
-print(john)
+ivan = Person('Иван Petrov', 22, 'Apple', 2500000)
+john = Person('John Sidorov', 24, 'ФСБ', 1000000)
+print(ivan.age)
+print(john.name)
 print(ivan.lastName(), john.lastName())
 
 
